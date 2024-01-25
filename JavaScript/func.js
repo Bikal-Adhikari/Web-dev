c8a22a0053fcb8afee1a59bbd0c31582c4b52e45
Bikal Adhikari <Bikal Adhikari>
2024-01-25 18:59:51 +1100

    JavaScript-function

--- JavaScript/func.js
+++ JavaScript/func.js
@@ -32,3 +32,49 @@ function main(){
            putBeeper();
            turnRight();
            }
+
+
+    // Chess Board
+
+    function main(){
+        beepersRight();
+        goUpTurnLeft();
+        beepersLeft();
+        goUpTurnRight();
+           beepersRight();
+        goUpTurnLeft();
+        beepersLeft();
+        goUpTurnRight();
+           beepersRight();
+     }
+      
+     function goUpTurnRight() {
+        turnRight();
+        move();
+        turnRight();
+     }
+      
+     function goUpTurnLeft() {
+        turnLeft();
+        move();
+        turnLeft();
+     }
+      
+     function beepersRight() {
+        putBeeper();
+        move();
+        move();
+        putBeeper();
+        move();
+        move();
+        putBeeper();  
+     }
+      
+     function beepersLeft() {
+        move();
+        putBeeper();
+        move();
+        move();
+        putBeeper();
+        move();
+     }
