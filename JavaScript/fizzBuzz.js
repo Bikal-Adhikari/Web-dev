28a2b2c373cffbd203487e06007f79425dac4c31
Bikal <bikaladhikari5@gmail.com>
2024-03-15 14:52:22 +1100

    bottles of drinks

--- JavaScript/fizzBuzz.js
+++ JavaScript/fizzBuzz.js
@@ -8,16 +8,18 @@ var count = 1;
 // output.push(2); // always add item in the end
 
 function FizzBuzz() {
-  if (count % 3 === 0 && count % 5 === 0) {
-    output.push("FizzBuzz");
-  } else if (count % 3 === 0) {
-    output.push("Fizz");
-  } else if (count % 5 === 0) {
-    output.push("Buzz");
-  } else {
-    output.push(count);
-  }
+  while (count <= 100) {
+    if (count % 3 === 0 && count % 5 === 0) {
+      output.push("FizzBuzz");
+    } else if (count % 3 === 0) {
+      output.push("Fizz");
+    } else if (count % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(count);
+    }
 
-  count++;
+    count++;
+  }
   console.log(output);
 }
