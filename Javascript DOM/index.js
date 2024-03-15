162b4a5a65260dfe2bca233dabfea61c7b382e17
Bikal <bikaladhikari5@gmail.com>
2024-03-15 16:14:12 +1100

    DOM js

---
+++ Javascript DOM/index.js
@@ -0,0 +1,19 @@
+document.querySelector("h1").innerHTML = "Good Bye";
+// document.querySelector("h1").style.color = "purple";
+document.querySelector("h1").style.fontSize = "10rem";
+document.querySelector("h1").style.padding = "30%";
+
+document.querySelector("button").style.background = "yellow";
+
+document.firstElementChild.lastElementChild.querySelector(
+  "ul"
+).lastElementChild.innerHTML = "Bikal";
+
+document.querySelector("li a").style.color = "orange";
+
+document.querySelector("button").classList.add("invisible");
+document.querySelector("button").classList.remove("invisible");
+document.querySelector("button").classList.toggle("invisible");
+document.querySelector("button").classList.toggle("invisible");
+
+document.querySelector("h1").classList.add("huge");
