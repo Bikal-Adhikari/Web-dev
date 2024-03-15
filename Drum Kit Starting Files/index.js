716e7295e10b65b9df5d42af80c9de2062e730b9
Bikal <bikaladhikari5@gmail.com>
2024-03-15 17:03:55 +1100

    Drum kit challenge

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,6 @@
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for (let i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    alert("I got clicked!");
+  });
+}
